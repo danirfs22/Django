711e7adaa67b7300f94d025f03ace2cafc0a9ca0
danirfs22 <121183358+danirfs22@users.noreply.github.com>
2025-12-02 19:46:45 +0000

    Blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Post
 
 def post_list(request):
-    return render(request, "blog/post_list.html", {})
+    posts = Post.objects.order_by("-create_date")
+    return render(request, "blog/post_list.html", {'post_list': posts})
 
