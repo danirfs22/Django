e61a56f4e8770c5310ec492c29835708426fc8eb
danirfs22 <121183358+danirfs22@users.noreply.github.com>
2025-12-04 19:31:04 +0000

    blog

--- dani_blog/settings.py
+++ dani_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 CSRF_TRUSTED_ORIGINS = [
     'https://localhost:8000', 
     'https://127.0.0.1:8000' 
