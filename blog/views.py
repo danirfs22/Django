502f702173bb40ae75db97a2b8292a8fb466cf92
danirfs22 <121183358+danirfs22@users.noreply.github.com>
2025-12-09 19:54:12 +0000

    blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def post_list(request):
-    posts = Post.objects.order_by("-create_date")
+    posts = Post.objects.order_by("-published_date")
     return render(request, "blog/post_list.html", {'post_list': posts})
 
+def post_detail(request, pk):
+    post= get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post':post})
+
+    def post_new(request):
+        form=PostForm()
+        return render(request, 'blog/post_edit.html', {'form':form})
